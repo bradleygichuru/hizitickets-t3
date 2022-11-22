7dee18e6a0cf9dd51a925c4f77c7c9cf39dc08e2
bradleygichuru <prz03@protonmail.com>
2022-11-22 06:57:17 +0000

    attempting to fix auth once more

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -24,6 +24,9 @@ export const authOptions: NextAuthOptions = {
     }),
     // ...add more providers here
   ],
+  pages: {
+    signIn: "/auth/signin",
+  },
 };
 
 export default NextAuth(authOptions);
