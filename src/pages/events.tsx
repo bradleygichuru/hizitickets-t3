71b89a1d7cf83c316b3a118e80882bae8d15f152
bradleygichuru <prz03@protonmail.com>
2022-12-16 22:43:20 +0000

    feat: adds no data display function

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,17 +1,26 @@
-import {NextPage} from "next";
+import { NextPage } from "next";
 import Layout from "../components/layout";
 import { trpc } from "../utils/trpc";
 import Section from "../components/section";
 import ReactLoading from "react-loading";
+import Image from "next/image";
+import ticket from "../../public/ticket-svgrepo-com.svg";
 const EventsPage: NextPage = () => {
   const { data, isLoading } = trpc.events.getEvents.useQuery();
   console.log(data);
   if (isLoading) {
     return (
       <div className="grid h-screen place-items-center bg-base-100 text-base-content">
-        <ReactLoading type="spin" color="#0000FF" height={100} width={100}  />
+        <ReactLoading type="spin" color="#0000FF" height={100} width={100} />
       </div>
     );
+  }if(data?.events.length == 0){
+      return (
+    <Layout>
+      <div className="grid h-screen place-items-center font-extrabold bg-base-100  text-xl m-10 text-base-content">
+          <Image src={ticket} width={200} alt="ticket" height={200} />
+          No Events</div> 
+    </Layout>);
   }
   return (
     <Layout>
