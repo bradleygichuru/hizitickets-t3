0a448e1e55f3bee3734381aa6f8c7ffcae2f29e2
bradleygichuru <prz03@protonmail.com>
2022-12-22 15:32:40 +0000

    feat: adds admin event verification

---
+++ src/components/EventValidationEntry.tsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import { trpc } from "../utils/trpc";
+
+import { useToast } from "@chakra-ui/react";
+const EventInfo = (props: { EventValidity: boolean; EventName: string }) => {
+  const toast = useToast();
+  const [eventValidity, setEventValidity] = useState<boolean>();
+  const verifyMutation = trpc.event.verifyEvent.useMutation();
+  useEffect(() => {
+    setEventValidity(props?.EventValidity);
+  }, []);
+  return (
+    <div className="stats shadow">
+      <div className="stat">
+        <div className="stat-title">Verified: {`${props?.EventValidity}`}</div>
+        <div className="stat-value">Event Name: {props?.EventName}</div>
+        <div className="stat-desc inline-flex">
+          Valid{" "}
+          <input
+            type="checkbox"
+            onChange={() => setEventValidity((prev) => !prev)}
+            className="toggle mx-4 toggle-xs toggle-success"
+            checked={eventValidity}
+          />
+        </div>
+
+        <button
+          className="m-2 btn btn-accent btn-wide"
+          onClick={() => {
+            verifyMutation
+              .mutateAsync({ eventName: props?.EventName })
+              .then(({ verification }) => {
+                if (verification == "successful") {
+                  toast({
+                    title: "Success.",
+                    description: "Your event was verified successfully",
+                    status: "success",
+                    duration: 9000,
+                    isClosable: true,
+                  });
+                } else {
+                  toast({
+                    title: "Error.",
+                    description: "There was an error verifying your event",
+                    status: "error",
+                    duration: 9000,
+                    isClosable: true,
+                  });
+                }
+              });
+          }}
+          disabled={props.EventValidity}
+        >
+          Verify
+        </button>
+      </div>
+    </div>
+  );
+};
+export default EventInfo;
---
+++ src/pages/admin.tsx
@@ -0,0 +1,46 @@
+import { trpc } from "../utils/trpc";
+import Image from "next/image";
+import error from "../../public/error.svg";
+import { useSession, signIn } from "next-auth/react";
+import ReactLoading from "react-loading";
+import EventInfo from "../components/EventValidationEntry";
+const AdminPage = () => {
+  const { data: session, status } = useSession();
+  const { data, isLoading } = trpc.events.getEvents.useQuery();
+
+  if (status == "unauthenticated") {
+    signIn();
+  }
+
+  if (status != "authenticated") {
+    return (
+      <div className=" grid h-screen place-items-center bg-base-100">
+        <ReactLoading type="spin" color="#0000FF" height={100} width={100} />
+        <span className="text-black">Authenticating</span>
+      </div>
+    );
+  }
+  if (session?.user?.email == "bradleygichuru@gmail.com") {
+    return (
+      <div className="grid h-screen place-items-center bg-base-100">
+        {data?.events?.map((event, index) => {
+          return (
+            <EventInfo key={index}
+              EventValidity={event?.EventValidity}
+              EventName={event?.EventName}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+  return (
+    <div className=" grid h-screen place-items-center bg-base-100">
+      <Image src={error} />
+      <span className="text-black">
+        You are not authorized to view this page
+      </span>
+    </div>
+  );
+};
+export default AdminPage;
