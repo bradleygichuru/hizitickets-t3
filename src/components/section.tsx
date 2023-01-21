35f1c7163d712d96d627f58fcd2a98583ed9a62f
bradleygichuru <prz03@protonmail.com>
2023-01-22 00:16:02 +0300

    refactor: refactors unwanted span

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -11,7 +11,6 @@ export default function Section(props: {
     <div className="flex flex-col content-center ">
       <div className="container ml-12 mt-8 flex w-auto sm:ml-24 sm:w-auto">
         <span className="font-sans text-sm font-bold ">
-          {props.sectionName}
         </span>
       </div>
       <div className="mt-20 ml-10 mr-10 grid w-auto grid-cols-1 justify-center rounded-xl bg-base-100  p-3 sm:ml-24 sm:mt-20 sm:grid sm:grid-cols-4 sm:grid-rows-1 ">
