06a8c11ff53c28cd73160442f5f384507400b653
bradleygichuru <bradleygichuru@gmail.com>
2024-06-20 14:53:07 +0300

    feat:adds analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { trpc } from "../utils/trpc";
-
+import { Analytics } from "@vercel/analytics/react"
 import "../styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
@@ -11,9 +11,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => {
   return (
+
     <SessionProvider session={session}>
       <ChakraProvider>
         <Component {...pageProps} />
+        <Analytics/>
       </ChakraProvider>
     </SessionProvider>
   );
